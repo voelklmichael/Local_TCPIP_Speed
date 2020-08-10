63009dccc84f76142b4b5a74fa2a3798f2d5fbb8
Michael <v.mi@gmx.de>
2020-08-10 11:26:47 +0200

    created rust crate

---
+++ LocalNetworkSpeed/target/debug/deps/LocalNetworkSpeed-ade6f0584d3fa0ba.d
@@ -0,0 +1,5 @@
+/home/michael/Local_TCPIP_Speed/LocalNetworkSpeed/target/debug/deps/LocalNetworkSpeed-ade6f0584d3fa0ba.rmeta: src/main.rs
+
+/home/michael/Local_TCPIP_Speed/LocalNetworkSpeed/target/debug/deps/LocalNetworkSpeed-ade6f0584d3fa0ba.d: src/main.rs
+
+src/main.rs:
